ebe97ad170427d6c263ed4ba8847f18ab2051cae
skjanyou <834832380@qq.com>
2019-12-21 23:18:19 +0800

    完成mysql的查询参数动态绑定和结果动态映射

--- com.skjanyou.db.mybatis/src/test/java/com/skjanyou/db/mybatis/AppTest.java
+++ com.skjanyou.db.mybatis/src/test/java/com/skjanyou/db/mybatis/AppTest.java
@@ -1,5 +1,7 @@
 package com.skjanyou.db.mybatis;
 
+import com.skjanyou.db.mybatis.util.DBUtil;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,32 +9,29 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
+public class AppTest extends TestCase {
+    public AppTest( String testName ){
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
+    public static Test suite(){
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testMybatis(){
+    	String className = "com.mysql.cj.jdbc.Driver";
+    	String url = "jdbc:mysql://127.0.0.1:3306/blog?zeroDateTimeBehavior=CONVERT_TO_NULL&useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC";
+    	String user = "root";
+    	String password = "123456";
+    	DBUtil.init(className, url, user, password);
+    	
+    	String sql = "select * from user where user_id = #user_id# and user_name = #user_name# and user_pass = #user_pass#";
+    	User userBean = new User();
+    	userBean.setUser_id("skjanyou");
+    	userBean.setUser_name("skjanyou");
+    	userBean.setUser_pass("12345678");
+    	User selectUser = DBUtil.get().executeSelectSql(sql, userBean, User.class);
+    	System.out.println(selectUser.getUser_id());
+        assertNotNull(selectUser);
     }
 }
---
+++ com.skjanyou.db.mybatis/src/test/java/com/skjanyou/db/mybatis/User.java
@@ -0,0 +1,26 @@
+package com.skjanyou.db.mybatis;
+
+public class User {
+	private String user_id;
+	private String user_name;
+	private String user_pass;
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getUser_pass() {
+		return user_pass;
+	}
+	public void setUser_pass(String user_pass) {
+		this.user_pass = user_pass;
+	}
+	
+}
