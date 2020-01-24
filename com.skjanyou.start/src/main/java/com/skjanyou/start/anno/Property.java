6bdba3a2089923e57b8a97c3002f6f16ad51f2a9
skjanyou <skjanyou@163.com>
2020-01-24 13:40:52 +0800

    修改注解

--- com.skjanyou.start/src/main/java/com/skjanyou/start/anno/Property.java
+++ com.skjanyou.start/src/main/java/com/skjanyou/start/anno/Property.java
@@ -6,14 +6,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import com.skjanyou.start.config.ConfigManagerFactory;
-import com.skjanyou.start.config.impl.WithoutConfig;
-
-
-@Target( { ElementType.TYPE } )
+@Target( { ElementType.TYPE,ElementType.FIELD } )
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface Configure {
-	String name();
-	Class<? extends ConfigManagerFactory> configManagerFactory() default WithoutConfig.class;
+public @interface Property {
+	String value() ;
 }
