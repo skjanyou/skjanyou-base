36c34f44ff8ac1d3b6c611ec6ee1f9e9a0b9e486
skjanyou <skjanyou@163.com>
2020-02-20 10:53:51 +0800

    删除无用代码

--- com.skjanyou.db.mybatis/src/main/java/com/skjanyou/db/mybatis/plugin/MybatisPlugin.java
+++ com.skjanyou.db.mybatis/src/main/java/com/skjanyou/db/mybatis/plugin/MybatisPlugin.java
@@ -14,28 +14,9 @@ import com.skjanyou.plugin.constant.DefineAnnotationClassPosition;
 import com.skjanyou.util.CommUtil;
 
 public class MybatisPlugin implements PluginSupport {
-
-	
-	class definedScanClass {
-		public Class<? extends Annotation> defineClass() {
-			return Mapper.class;
-		};
-		
-		// 对扫描到的类有以下几种处理方式
-		// 1.创建实例Bean,放入Bean容器
-		// 2.创建代理Bean,放入Bean容器
-		// 3.扫描获得类上面的注解,加载某些配置
-		// 4.通过class的信息对系统进行配置
-		// 5.其他情况
-		// 这个方法与init方法的区别在于,init扫描到的是plugin内部的class,而classProcess则是系统中所有满足注解的类
-		public void classProcess(){};
-		// 扫描类内部的注解,最好要求在类上面添加一个注解,以便减少检索范围
-		public void fieldProcess(){};
-	}
 	
 	@Override
 	public void init(List<Class<?>> plugnInnerClass,PluginConfig properties) {
-		System.out.println("mybatis插件初始化!");
 		PluginDefineAnnotationClassManager.regist(new PluginDefineAnnotationClassAdapter() {
 			@Override
 			public Class<? extends Annotation> defineClass() {
