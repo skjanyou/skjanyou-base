79775bb7adeab9a4f8e1ad3c32b68688db61ff8a
skjanyou <skjanyou@163.com>
2020-11-04 23:26:25 +0800

    添加窗体边缘鼠标类型切换功能

--- com.skjanyou.javafx/src/main/java/com/skjanyou/javafx/inter/impl/JavaFxBlackDecorator.java
+++ com.skjanyou.javafx/src/main/java/com/skjanyou/javafx/inter/impl/JavaFxBlackDecorator.java
@@ -1,6 +1,7 @@
 package com.skjanyou.javafx.inter.impl;
 
 import com.skjanyou.javafx.inter.JavaFxDecorator;
+import com.skjanyou.javafx.util.DragUtil;
 
 import javafx.application.Platform;
 import javafx.beans.InvalidationListener;
@@ -9,7 +10,6 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
@@ -126,93 +126,7 @@ public class JavaFxBlackDecorator implements JavaFxDecorator {
 
 
 
-		stageRoot.setOnMouseMoved(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				event.consume();
-				double x = event.getSceneX();
-				double y = event.getSceneY();
-				double width = stage.getWidth();
-				double height = stage.getHeight();
-				Cursor cursorType = Cursor.DEFAULT;// 鼠标光标初始为默认类型，若未进入调整窗口状态，保持默认类型
-
-				// 先将所有调整窗口状态重置
-				isRight = isBottomRight = isBottom = false;
-				if (y >= height - RESIZE_WIDTH) {
-
-					if (x <= RESIZE_WIDTH) {// 左下角调整窗口状态
-
-					} else if (x >= width - RESIZE_WIDTH) {// 右下角调整窗口状态
-						isBottomRight = true;
-						cursorType = Cursor.SE_RESIZE;
-					} else {// 下边界调整窗口状态
-						isBottom = true;
-						cursorType = Cursor.S_RESIZE;
-					}
-				} else if (x >= width - RESIZE_WIDTH) {// 右边界调整窗口状态
-					isRight = true;
-					cursorType = Cursor.E_RESIZE;
-				}
-
-				// 最后改变鼠标光标
-				stageRoot.setCursor(cursorType);
-			}
-		});
-
-
-
-		stageRoot.setOnMouseDragged(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				double x = event.getSceneX();
-				double y = event.getSceneY();
-
-				// 保存窗口改变后的x、y坐标和宽度、高度，用于预判是否会小于最小宽度、最小高度
-				double nextX = stage.getX();
-				double nextY = stage.getY();
-				double nextWidth = stage.getWidth();
-				double nextHeight = stage.getHeight();
-
-				if (isRight || isBottomRight) {// 所有右边调整窗口状态
-					nextWidth = x;
-				}
-
-				if (isBottomRight || isBottom) {// 所有下边调整窗口状态
-					nextHeight = y;
-				}
-
-				if (nextWidth <= MIN_WIDTH) {// 如果窗口改变后的宽度小于最小宽度，则宽度调整到最小宽度
-					nextWidth = MIN_WIDTH;
-				}
-
-				if (nextHeight <= MIN_HEIGHT) {// 如果窗口改变后的高度小于最小高度，则高度调整到最小高度
-					nextHeight = MIN_HEIGHT;
-				}
-
-				// 最后统一改变窗口的x、y坐标和宽度、高度，可以防止刷新频繁出现的屏闪情况
-				stage.setX(nextX);
-				stage.setY(nextY);
-				stage.setWidth(nextWidth);
-				stage.setHeight(nextHeight);
-
-				if(!isBottom && !isBottomRight && !isRight) {
-					stage.setX(event.getScreenX() - xOffset);
-					stage.setY(event.getScreenY() - yOffset);
-				}
-
-			}
-		});
-
-
-
-		stageRoot.setOnMousePressed(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				xOffset = event.getSceneX();
-				yOffset = event.getSceneY();
-			}
-		});
-
+		DragUtil.addDrawFunc(stage, stageRoot);
 
 	}
 
---
+++ com.skjanyou.javafx/src/main/java/com/skjanyou/javafx/util/DragUtil.java
@@ -0,0 +1,115 @@
+package com.skjanyou.javafx.util;
+
+import javafx.event.EventHandler;
+import javafx.scene.Cursor;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ * 拉伸工具类
+ * @author Light
+ */
+public class DragUtil {
+    enum  DIRECTION {
+    	NONE,TOP,RIGHT,BOTTOM,LEFT,TOP_RIGHT,RIGHT_BOTTOM,LEFT_BOTTOM,TOP_LEFT
+    } 
+    private static DIRECTION direction = DIRECTION.NONE;
+    private static Cursor curorType = Cursor.DEFAULT;
+    private static boolean isPressdown;
+    
+	//窗体拉伸属性
+    private final static int RESIZE_WIDTH = 5;// 判定是否为调整窗口状态的范围与边界距离
+    private final static double MIN_WIDTH = 300;// 窗口最小宽度
+    private final static double MIN_HEIGHT = 250;// 窗口最小高度
+    
+    public static void addDrawFunc(Stage stage,VBox root) {
+        
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				isPressdown = true;
+			}
+		});
+        
+        root.setOnMouseReleased(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				isPressdown = false;
+			}
+		});
+        
+        
+        stage.addEventFilter(MouseEvent.MOUSE_MOVED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+	            double x = event.getSceneX();
+	            double y = event.getSceneY();
+	            double width = stage.getWidth();
+	            double height = stage.getHeight();
+	            Cursor cursorType = Cursor.DEFAULT;// 鼠标光标初始为默认类型，若未进入调整窗口状态，保持默认类型
+	            // 先将所有调整窗口状态重置
+	            direction = DIRECTION.NONE;
+	            if (y >= height - RESIZE_WIDTH) {
+	            	System.out.println("y+");
+	                if (x <= RESIZE_WIDTH) {// 左下角调整窗口状态
+	                	direction = DIRECTION.LEFT_BOTTOM;
+	                	curorType = Cursor.SW_RESIZE;
+	                } else if (x >= width - RESIZE_WIDTH) {// 右下角调整窗口状态
+	                	direction = DIRECTION.RIGHT_BOTTOM;
+	                    cursorType = Cursor.SE_RESIZE;
+	                } else {// 下边界调整窗口状态
+	                	direction = DIRECTION.BOTTOM;
+	                    cursorType = Cursor.S_RESIZE;
+	                }
+	            } else if( y <= RESIZE_WIDTH ){
+	                if (x <= RESIZE_WIDTH) {// 左上角调整窗口状态
+	                	direction = DIRECTION.TOP_LEFT;
+	                	curorType = Cursor.NW_RESIZE;
+	                } else if (x >= width - RESIZE_WIDTH) {// 右上角调整窗口状态
+	                	direction = DIRECTION.TOP_RIGHT;
+	                    cursorType = Cursor.NE_RESIZE;
+	                } else {// 下边界调整窗口状态
+	                	direction = DIRECTION.TOP;
+	                    cursorType = Cursor.N_RESIZE;
+	                }
+	            } else if (x >= width - RESIZE_WIDTH) {// 右边界调整窗口状态
+	            	direction = DIRECTION.RIGHT;
+	            	cursorType = Cursor.E_RESIZE;
+	            } else if (x <= RESIZE_WIDTH) {
+	            	direction = DIRECTION.LEFT;
+	            	cursorType = Cursor.W_RESIZE;
+	            }
+	            // 最后改变鼠标光标
+	            root.setCursor(cursorType);
+	            // 拖拽窗口改变大小
+	            if( isPressdown ) {
+	                // 保存窗口改变后的x、y坐标和宽度、高度，用于预判是否会小于最小宽度、最小高度
+	                double nextX = stage.getX();
+	                double nextY = stage.getY();
+	                System.out.println("nextX:" + nextX + ",nextY:" + nextY);
+	                double nextWidth = stage.getWidth();
+	                double nextHeight = stage.getHeight();
+	                
+	                if (direction == DIRECTION.RIGHT || direction == DIRECTION.RIGHT_BOTTOM) {// 所有右边调整窗口状态
+	                    nextWidth = x;
+	                }
+	                if (direction == DIRECTION.BOTTOM || direction == DIRECTION.RIGHT_BOTTOM || direction == DIRECTION.LEFT_BOTTOM) {// 所有下边调整窗口状态
+	                    nextHeight = y;
+	                }
+	                if (nextWidth <= MIN_WIDTH) {// 如果窗口改变后的宽度小于最小宽度，则宽度调整到最小宽度
+	                    nextWidth = MIN_WIDTH;
+	                }
+	                if (nextHeight <= MIN_HEIGHT) {// 如果窗口改变后的高度小于最小高度，则高度调整到最小高度
+	                    nextHeight = MIN_HEIGHT;
+	                }
+	                // 最后统一改变窗口的x、y坐标和宽度、高度，可以防止刷新频繁出现的屏闪情况
+	                stage.setX(nextX);
+	                stage.setY(nextY);
+	                stage.setWidth(nextWidth);
+	                stage.setHeight(nextHeight);	                
+	            }
+			}
+		});
+    }
+}
