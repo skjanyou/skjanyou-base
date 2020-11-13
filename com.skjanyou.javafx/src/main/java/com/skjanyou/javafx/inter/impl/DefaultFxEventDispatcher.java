459f66c7dcf26e8ca8c5ffe074d8cdb2fc50e2d6
skjanyou <skjanyou@163.com>
2020-11-14 06:17:23 +0800

    添加事件分发器的缓存功能

--- com.skjanyou.javafx/src/main/java/com/skjanyou/javafx/inter/impl/DefaultFxEventDispatcher.java
+++ com.skjanyou.javafx/src/main/java/com/skjanyou/javafx/inter/impl/DefaultFxEventDispatcher.java
@@ -12,17 +12,16 @@ import java.util.Set;
 import com.skjanyou.javafx.anno.FxAnnotation.FxEventHandler;
 import com.skjanyou.javafx.inter.FxEventDispatcher;
 import com.skjanyou.util.MethodUtil;
-import com.skjanyou.util.StringUtil;
 import com.sun.javafx.css.Selector;
 
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.text.Text;
 
 
 /**
+ * <pre>
  * 事件分发器,基于过滤器的父组件事件分发器
  * 1.先搜索类中的所有事件,对事件进行分组,在捕获到不在其中的事件时,不进行处理（提升效率）  完成
  * 2.收集所有的选择器，按照事件,对选择器进行分组
@@ -30,7 +29,7 @@ import javafx.scene.text.Text;
  * 4.遍历当前目标组件以及父组件，通过选择器进行匹配，找到满足要求的目标组件
  * 5.替换event对象中的源组件
  * 6.调用类中的handler进行处理
- * 
+ * </pre>
  * @author skjanyou
  *
  */
@@ -41,6 +40,8 @@ public class DefaultFxEventDispatcher implements FxEventDispatcher,EventHandler<
 	protected Set<String> eventTypeSet; 		//  保存所有的事件类型
 	protected Map<String,Set<Selector>> eventTypeAndSelectorMap;   // 事件类型与选择器的映射
 	protected Map<Selector,Set<Method>> selectorMethodMap;	// 保存选择器对应的所有方法
+	
+	protected Map<String,Map<Selector,List<Method>>> mappingCache;   // 缓存,初次加载之后会缓存事件监听器，后续再次调用时,可以通过缓存查找，加快效率
 
 	public DefaultFxEventDispatcher(Object controller, Class<?> targetClass) {
 		this.controller = controller;
@@ -50,7 +51,6 @@ public class DefaultFxEventDispatcher implements FxEventDispatcher,EventHandler<
 		}
 		this.initMethodCollection();
 		this.resolveClass();
-		System.out.println(this);
 	}
 
 
@@ -59,6 +59,7 @@ public class DefaultFxEventDispatcher implements FxEventDispatcher,EventHandler<
 		this.eventTypeSet = new HashSet<>();
 		this.eventTypeAndSelectorMap = new HashMap<>();
 		this.selectorMethodMap = new HashMap<>();
+		this.mappingCache = new HashMap<>();
 	}
 
 	/** 解析类中的方法 **/
@@ -99,20 +100,6 @@ public class DefaultFxEventDispatcher implements FxEventDispatcher,EventHandler<
 		}
 
 	}
-
-	private void checkAndFillMap( Map<String,List<Method>> targetMethodMethod,String key,Method tMethod ) {
-		if( !StringUtil.isBlank(key) ) {
-			List<Method> tempList = null; 
-			tempList = targetMethodMethod.get(key);
-			if( tempList == null ) {
-				tempList = new ArrayList<>();
-				tempList.add(tMethod);
-				targetMethodMethod.put(key, tempList);
-			}else {
-				tempList.add(tMethod);
-			}
-		}
-	}
 	
 	@Override
 	public EventHandler<Event> getEventHandler() {
@@ -125,6 +112,7 @@ public class DefaultFxEventDispatcher implements FxEventDispatcher,EventHandler<
 		String eventTypeName = event.getEventType().getName();
 		// 过滤掉未捕获事件
 		if( !this.eventTypeSet.contains(eventTypeName) ) { return ; }
+		
 		// 当前要分发的事件对于的选择器
 		Set<Selector> selectorList = this.eventTypeAndSelectorMap.get(eventTypeName);
 		
@@ -133,29 +121,53 @@ public class DefaultFxEventDispatcher implements FxEventDispatcher,EventHandler<
 			// 遍历当前目标组件以及父组件，通过选择器进行匹配，找到满足要求的目标组件
 			// 事件目标
 			Object target = event.getTarget();
-			loopMatcherSelector(target,selector,event);
+			// 1.在缓存中查找
+			boolean hasHit = false;
+			Map<Selector,List<Method>> map = this.mappingCache.get(eventTypeName);
+			if( map != null ) {
+				List<Method> methodList = map.get(selector);
+				if( methodList != null && methodList.size() != 0 ) {
+					Event newEvent = event;
+					Node parent = null;
+					// 处理一下事件对象
+					if( target instanceof Node ){
+						parent = (Node) target;
+						if( target instanceof Text) {
+							// Button之类的组件上面的文字,需要获取Parent作为事件目标,并用来做匹配
+							Text tt = (Text) target;
+							parent = tt.getParent();
+							newEvent = event.copyFor(event.getSource(), parent);
+						}			
+					}
+					if( selector.applies(parent) ) {
+						hasHit = true;
+						
+						// 触发绑定选择器的方法
+						for( Method tMethod : methodList ) {
+							MethodUtil.invokeAndIgnoreException(tMethod, controller, new Object[]{ newEvent });
+						}
+					}
+				}
+			}
+			
+			// 2.缓存未命中,继续解析
+			if( !hasHit ) {
+				List<Method> result = loopMatcherSelector(target,selector,event);
+				Map<Selector,List<Method>> selectorMap = this.mappingCache.get(eventTypeName);
+				if( selectorMap == null ) {
+					selectorMap = new HashMap<>();
+				}
+				selectorMap.put(selector, result);
+				this.mappingCache.put(eventTypeName, selectorMap);
+			}
 		}
-		
-		
-
-		
 	}
 	
 
-	private void loopMatcherSelector( Object target,Selector selector,Event event ) {
+	private List<Method> loopMatcherSelector( Object target,Selector selector,Event event ) {
+		List<Method> result = new ArrayList<>();
 		Node parent = null;
 		Event newEvent = event;
-//		if( target instanceof Parent ) {
-//			parent = (Parent) target;
-//		} else if( target instanceof Text) {
-//			// Button之类的组件上面的文字,需要获取Parent作为事件目标,并用来做匹配
-//			Text tt = (Text) target;
-//			parent = tt.getParent();
-//			newEvent = event.copyFor(event.getSource(), parent);
-//		}else if( target instanceof Node ){
-//			Node node = (Node) target;
-//			parent = node.getParent();
-//		}
 		
 		if( target instanceof Node ){
 			parent = (Node) target;
@@ -166,13 +178,10 @@ public class DefaultFxEventDispatcher implements FxEventDispatcher,EventHandler<
 				newEvent = event.copyFor(event.getSource(), parent);
 			}			
 		}		
-		
-		
+		// 递归临界值
 		if( parent == null || parent.getStyleableParent() == null ) {
-			return ;
+			return result;
 		}
-		
-		
 		
 		// 待匹配搜索到的方法,全部找出来再执行,免得多个选择器的情况下,方法执行间隔过长
 		Set<Method> targetMethod = new HashSet<>();
@@ -182,13 +191,18 @@ public class DefaultFxEventDispatcher implements FxEventDispatcher,EventHandler<
 			targetMethod.addAll(methods);
 		}
 		
-		// 3.触发绑定选择器的方法
+		// 触发绑定选择器的方法
 		for( Method tMethod : targetMethod ) {
 			MethodUtil.invokeAndIgnoreException(tMethod, controller, new Object[]{ newEvent });
 		}
+
+		result = new ArrayList<>(targetMethod);
 		
 		// 递归调用父组件
-		loopMatcherSelector(parent.getParent(),selector,event);
+		List<Method> parentResult = loopMatcherSelector(parent.getParent(),selector,event);
+		result.addAll(parentResult);
+		
+		return result;
 	}
 
 }
