3bd03c009d4d431e167c4c4dbf85f3990107ad56
skjanyou <skjanyou@163.com>
2021-01-23 21:55:32 +0800

    重新拆解一下MvcHandler

--- com.skjanyou.mvc/src/main/java/com/skjanyou/mvc/handler/MvcHandler.java
+++ com.skjanyou.mvc/src/main/java/com/skjanyou/mvc/handler/MvcHandler.java
@@ -1,15 +1,10 @@
 package com.skjanyou.mvc.handler;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Proxy;
 import java.lang.reflect.Type;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -18,39 +13,26 @@ import com.skjanyou.annotation.api.Web.ResponseBody;
 import com.skjanyou.annotation.api.enumclass.ResponseType;
 import com.skjanyou.log.core.Logger;
 import com.skjanyou.log.util.LogUtil;
-import com.skjanyou.mvc.anno.Mvc.Autowired;
-import com.skjanyou.mvc.anno.Mvc.Controller;
-import com.skjanyou.mvc.anno.Mvc.Dao;
 import com.skjanyou.mvc.anno.Mvc.HttpParameter;
 import com.skjanyou.mvc.anno.Mvc.HttpPostReuqestBody;
-import com.skjanyou.mvc.anno.Mvc.Mapping;
-import com.skjanyou.mvc.anno.Mvc.Service;
 import com.skjanyou.mvc.bean.Context;
-import com.skjanyou.mvc.core.MvcApplicateContext;
 import com.skjanyou.server.api.constant.StatusCode;
 import com.skjanyou.server.api.exception.ServerException;
 import com.skjanyou.server.api.inter.Request.RequestLine;
-import com.skjanyou.server.api.inter.ServerHandler;
 import com.skjanyou.server.core.HttpHeaders;
 import com.skjanyou.server.core.HttpProtocolLv1;
 import com.skjanyou.server.core.HttpRequest;
 import com.skjanyou.server.core.HttpResponse;
 import com.skjanyou.server.core.HttpResponse.HttpResponseBody;
 import com.skjanyou.server.core.HttpResponse.HttpResponseLine;
-import com.skjanyou.server.simplehttpserver.http.HttpServerHandler;
-import com.skjanyou.util.ClassUtil;
 import com.skjanyou.util.StringUtil;
 import com.skjanyou.util.convert.ConvertUtil;
 
-public class MvcHandler extends HttpServerHandler {
+public class MvcHandler extends MappingHandler {
 	private Logger logger = LogUtil.getLogger(MvcHandler.class);
-	// url映射
-	private Map<String,Context> mappings = new HashMap<>();
-	// 扫描的class
-	private String scanPath = null;
 	
 	public MvcHandler( String scanPath ) {
-		this.scanPath = scanPath;
+		super(scanPath);
 	}
 	
 	@Override
@@ -202,113 +184,7 @@ public class MvcHandler extends HttpServerHandler {
 		
 	}	
 	
-	@Override
-	public ServerHandler init() throws ServerException {
-		initBean();
-		autowired();
-		return this;
-	}
-
 	
-	private void initBean() throws ServerException{
-		List<Class<?>> list = ClassUtil.getClasses(this.scanPath);
-		for ( Class<?> clazz : list ) {
-			// 接口,跳过处理
-			if(clazz.isInterface()){ continue; }
-			
-			try{
-				// 包含Service
-				Service service = clazz.getDeclaredAnnotation(Service.class);
-				if( service != null ){
-					Object object = clazz.newInstance();
-					String serviceName = service.value();
-					MvcApplicateContext.putBean(serviceName, object);				
-				}
-				
-				// 包含Dao
-				Dao dao = clazz.getDeclaredAnnotation(Dao.class);
-				if( dao != null ){
-					Object object = clazz.newInstance();
-					String daoName = dao.value();
-					MvcApplicateContext.putBean(daoName, object);
-				}			
-				
-				// 包含Controller注解
-				Controller controller = clazz.getDeclaredAnnotation(Controller.class);
-				if( controller != null ){
-					Object object = clazz.newInstance();
-					MvcApplicateContext.putBean(clazz, object);
-					// 构建映射关系
-					String namespace = controller.value();
-					Method[] methods = clazz.getDeclaredMethods();
-					Mapping mapping = null;
-					Context context = null;
-					for ( Method md : methods ) {
-						mapping = md.getDeclaredAnnotation(Mapping.class);
-						if( mapping != null ){
-							String m = mapping.value();
-							String key = ("/" + namespace + "/" + m).replaceAll("/+","/");
-							context = new Context(object, clazz, md);
-							mappings.put(key, context);
-						}
-					}
-				}
-			}catch( Exception e ){
-				throw new ServerException("初始化Bean[" + clazz.getName() + "]失败",e);
-			}
-		}
-	}
-
-	private void autowired() throws ServerException{
-		HashSet<?> beanSet = MvcApplicateContext.getAllBeans();
-		Field[] fileds = null;
-		for (Object obj : beanSet) {
-			
-			fileds = obj.getClass().getDeclaredFields();
-			for (Field field : fileds) {
-				field.setAccessible(true);
-				Object filedObj = null;
-				// 包含Service
-				Service service = field.getDeclaredAnnotation(Service.class);
-				if( service != null ){
-					String beanName = service.value();
-					filedObj = MvcApplicateContext.getBean(beanName);
-					if( filedObj == null ){
-						throw new ServerException(fileds + "待装配对象在容器中不存在!");
-					}
-				}
-				
-				// 包含Dao
-				Dao dao = field.getDeclaredAnnotation(Dao.class);
-				if( dao != null ){
-					String beanName = dao.value();
-					filedObj = MvcApplicateContext.getBean(beanName);
-					if( filedObj == null ){
-						throw new ServerException(field + "待装配对象在容器中不存在!");
-					}					
-				}	
-				
-				// 自动装配
-				Autowired autowired = field.getDeclaredAnnotation(Autowired.class);
-				if( autowired != null ){
-					filedObj = MvcApplicateContext.getBeanByInterfaceClass(field.getType());
-					if( filedObj == null ){
-						throw new ServerException(field + "待装配对象在容器中不存在!");
-					}						
-				}
-				
-				if( filedObj != null ){
-					try {
-						field.set(obj, filedObj);
-					} catch (Exception e) {
-						throw new ServerException("对象[" + obj + "]的成员变量["+ field + "]装配属性失败",e);
-					}
-				}
-			}
-		}
-	
-		
-	}
 	
 	public static Object getBeanByInterfaceClass( Class<?> targetClass , Map<String,Object> beanMap ){
 		Collection<Object> values = beanMap.values();
